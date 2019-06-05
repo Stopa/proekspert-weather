b1aeb05efbe5023adb70b9e87ddfd66bc7682b12
Stepan <stepan@ut.ee>
2019-06-05 22:47:47 +0300

    Some additional tweaking to create-react-app, added task reference

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './components/App';
+
+const root = document.getElementById('root');
+
+if (root) {
+  ReactDOM.render(<App />, root);
+} else {
+  throw new Error('Root element not found.');
+}
