9ab03f22b08ff4de3c591d01c9dae2a156205b6a
Stepan <stepan@ut.ee>
2019-06-08 15:10:03 +0200

    Implemented geolocation

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -7,17 +7,20 @@
 import React, { useCallback } from 'react';
 import { connect } from 'react-redux';
 
-import { setQuery } from '../../store/actions/ui';
+import { setQuery, setError } from '../../store/actions/ui';
 import { fetchForecast } from '../../store/actions/forecast';
 
 type Props = {
   query: string,
   changeQuery: Function,
   doSearch: Function,
+  doSetError: Function,
 }
 
 function SearchView(props: Props) {
-  const { query, changeQuery, doSearch } = props;
+  const {
+    query, changeQuery, doSearch, doSetError,
+  } = props;
 
   const changeListener = useCallback((event) => {
     changeQuery(event.currentTarget.value);
@@ -29,9 +32,37 @@ function SearchView(props: Props) {
     doSearch();
   }, [doSearch]);
 
+  const geolocationSuccess = useCallback((position) => {
+    const { coords: { latitude, longitude } } = position;
+
+    changeQuery([latitude, longitude].join(','));
+
+    doSearch();
+  }, [changeQuery, doSearch]);
+
+  const geolocationError = useCallback(() => {
+    doSetError('Could not get current position');
+  }, [doSetError]);
+
+  const gpsButtonClickListener = useCallback(() => {
+    if (!navigator || !navigator.geolocation) {
+      setError('Your browser does not support geolocation.');
+
+      return null;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      geolocationSuccess,
+      geolocationError,
+    );
+
+    return null;
+  }, [geolocationSuccess, geolocationError]);
+
   return (
     <form action="#" onSubmit={submitListener}>
       <input type="text" value={query || ''} onChange={changeListener} />
+      <button type="button" onClick={gpsButtonClickListener}>Geolocate me bro</button>
     </form>
   );
 }
@@ -48,6 +79,7 @@ function mapDispatchToProps(dispatch) {
   return {
     changeQuery: query => dispatch(setQuery(query)),
     doSearch: () => dispatch(fetchForecast()),
+    doSetError: message => dispatch(setError(message)),
   };
 }
 
