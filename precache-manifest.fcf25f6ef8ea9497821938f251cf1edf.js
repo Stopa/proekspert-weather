91b65b9115e4e3bb73ca6754755e4381a0e50761
Stepan <stepan@ut.ee>
2019-06-13 17:35:12 +0300

    Updates

---
+++ precache-manifest.fcf25f6ef8ea9497821938f251cf1edf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d66473bd3eea9b053fd3f12eaffd9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0662fa1ef17de5e3ca",
+    "url": "/static/css/2.fa08915d.chunk.css"
+  },
+  {
+    "revision": "2055155fd509ef725752",
+    "url": "/static/css/main.430b94c9.chunk.css"
+  },
+  {
+    "revision": "eb0662fa1ef17de5e3ca",
+    "url": "/static/js/2.fc4e17ff.chunk.js"
+  },
+  {
+    "revision": "2055155fd509ef725752",
+    "url": "/static/js/main.f63af24d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/proekspert-weather/precache-manifest.25c1f7e3cd29924864beb80193e52cda.js"
+  "/precache-manifest.fcf25f6ef8ea9497821938f251cf1edf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/proekspert-weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
